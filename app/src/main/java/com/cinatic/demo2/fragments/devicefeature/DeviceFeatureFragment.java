6aca15dc33cfac1206d06de95a06306162eef873
hieplebstar <hiep.le@bstarsolutions.com>
2017-04-10 13:16:00 +0700

    create sub feature menu

--- app/src/main/java/com/cinatic/demo2/fragments/devicefeature/DeviceFeatureFragment.java
+++ app/src/main/java/com/cinatic/demo2/fragments/devicefeature/DeviceFeatureFragment.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
 import com.badoo.mobile.util.WeakHandler;
 import com.cinatic.demo2.AppApplication;
@@ -28,13 +30,18 @@ import butterknife.BindView;
 import butterknife.OnClick;
 import butterknife.OnTouch;
 
-public class DeviceFeatureFragment extends ButterKnifeFragment{
+public class DeviceFeatureFragment extends ButterKnifeFragment {
 
     @BindView(R.id.linearlayout_feature_container_device_feature)
     LinearLayout mFeatureLinearLayout;
+    @BindView(R.id.imageview_menu_device_feature)
+    ImageView mMenuImageView;
+    @BindView(R.id.textview_tap_device_feature)
+    TextView mTabTextView;
 
-    FloatingActionMenu mMainFeatureMenu;
     private DeviceFeaturePresenter mPresenter;
+    private DeviceFeatureMenu mMenuMode;
+    private FloatingActionMenu mMainFeatureMenu, mMusicFeatureMenu;
 
     public static DeviceFeatureFragment newInstance() {
         DeviceFeatureFragment fragment = new DeviceFeatureFragment();
@@ -61,14 +68,19 @@ public class DeviceFeatureFragment extends ButterKnifeFragment{
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        if(mMainFeatureMenu == null) return;
-        mMainFeatureMenu.close(false);
-        mMainFeatureMenu = null;
+        if (mMainFeatureMenu != null) {
+            mMainFeatureMenu.close(false);
+            mMainFeatureMenu = null;
+        }
+        if (mMusicFeatureMenu != null) {
+            mMusicFeatureMenu.close(false);
+            mMusicFeatureMenu = null;
+        }
     }
 
     @OnTouch(R.id.linearlayout_feature_container_device_feature)
-    boolean onMenuTouch(View view, MotionEvent motionEvent){
-        switch (motionEvent.getAction()){
+    boolean onMenuTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mFeatureLinearLayout.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_hold_radius));
                 break;
@@ -80,17 +92,25 @@ public class DeviceFeatureFragment extends ButterKnifeFragment{
     }
 
     @OnClick(R.id.imageview_minimize_device_feature)
-    void onMinimizeClick(){
+    void onMinimizeClick() {
         mPresenter.showPreview();
     }
 
     public void initMenu() {
+        mMenuMode = DeviceFeatureMenu.MAIN_MENU;
         SubActionButton.Builder itemBuilder = new SubActionButton.Builder(getActivity());
 
         ImageView mMusicImageView = new ImageView(getActivity());
         mMusicImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_sound));
         SubActionButton mMusicSAB = itemBuilder.setContentView(mMusicImageView).build();
         mMusicSAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
+        mMusicSAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMenuMode = DeviceFeatureMenu.MUSIC_MENU;
+                updateMenu();
+            }
+        });
 
         ImageView mTempImageView = new ImageView(getActivity());
         mTempImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_temp));
@@ -112,6 +132,33 @@ public class DeviceFeatureFragment extends ButterKnifeFragment{
         SubActionButton mMuteSAB = itemBuilder.setContentView(mMuteImageView).build();
         mMuteSAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
 
+        ImageView mBackImageView = new ImageView(getActivity());
+        mBackImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_music));
+        SubActionButton mBackSAB = itemBuilder.setContentView(mBackImageView).build();
+        mBackSAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
+        mBackSAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMenuMode = DeviceFeatureMenu.MAIN_MENU;
+                updateMenu();
+            }
+        });
+
+        ImageView mPlayImageView = new ImageView(getActivity());
+        mPlayImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_play));
+        SubActionButton mPlaySAB = itemBuilder.setContentView(mPlayImageView).build();
+        mPlaySAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
+
+        ImageView mNextImageView = new ImageView(getActivity());
+        mNextImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_next));
+        SubActionButton mNextSAB = itemBuilder.setContentView(mNextImageView).build();
+        mNextSAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
+
+        ImageView mReverseImageView = new ImageView(getActivity());
+        mReverseImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_reverse));
+        SubActionButton mReverseSAB = itemBuilder.setContentView(mReverseImageView).build();
+        mReverseSAB.setBackground(AppApplication.getDrawableResource(R.drawable.menu_circle_accent_radius));
+
         mMainFeatureMenu = new FloatingActionMenu.Builder(getActivity())
                 .addSubActionView(mMusicSAB)
                 .addSubActionView(mTempSAB)
@@ -123,12 +170,39 @@ public class DeviceFeatureFragment extends ButterKnifeFragment{
                 .attachTo(mFeatureLinearLayout)
                 .build();
 
+        mMusicFeatureMenu = new FloatingActionMenu.Builder(getActivity())
+                .addSubActionView(mBackSAB)
+                .addSubActionView(mPlaySAB)
+                .addSubActionView(mNextSAB)
+                .addSubActionView(mReverseSAB)
+                .setStartAngle(-100)
+                .setEndAngle(10)
+                .attachTo(mFeatureLinearLayout)
+                .build();
+
         new WeakHandler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                if(mMainFeatureMenu == null) return;
-                mMainFeatureMenu.open(true);
+                updateMenu();
             }
         }, 200);
+    }
+
+    public void updateMenu() {
+        if (mMainFeatureMenu == null || mMusicFeatureMenu == null || mMenuImageView == null) return;
+        switch (mMenuMode) {
+            case MAIN_MENU:
+                mMainFeatureMenu.open(true);
+                mMusicFeatureMenu.close(false);
+                mMenuImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_mic));
+                mTabTextView.setVisibility(View.VISIBLE);
+                break;
+            case MUSIC_MENU:
+                mMusicFeatureMenu.open(true);
+                mMainFeatureMenu.close(false);
+                mMenuImageView.setImageDrawable(AppApplication.getDrawableResource(R.drawable.ic_sound));
+                mTabTextView.setVisibility(View.GONE);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/cinatic/demo2/fragments/devicefeature/DeviceFeatureMenu.java
@@ -0,0 +1,7 @@
+package com.cinatic.demo2.fragments.devicefeature;
+
+import java.io.Serializable;
+
+public enum DeviceFeatureMenu implements Serializable {
+    MAIN_MENU, MUSIC_MENU
+}
